9bcfdac447a313b5356a8f06700056fc7a8b9551
Jeffrey Bagdis <jeffrey.bagdis@shopify.com>
2020-05-05 15:15:56 -0400

    Re-add update script (#37)

--- install.sh
+++ install.sh
@@ -255,4 +255,6 @@ update() {
   update_powerlevel10k
   set_shell_to_zsh
   printf "${BOLD}Shell Environment successfully installed.\n${NORMAL}"
+  printf "${BLUE}Memoizing new profile... (ignore PowerLevel10k git prompt error.)\n${NORMAL}"
+  env zsh -l
 }
---
+++ update.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+source install.sh
+update
